7a8214f4a5dac29eed8fe690f71acaaf5033e2f4
notmoebius <notmoebius@gmail.com>
2019-04-15 15:33:34 +0200

    update app et fix user.rb

--- app.rb
+++ app.rb
@@ -7,12 +7,13 @@ alec = User.new('alec@alec.com', 20)
 
 puts "Liste d'utilisateurs : "
 puts User.all
-puts "L'utilisateur #{alec.email} a #{alec.age} ans"
+# puts "L'utilisateur #{alec.email} a #{alec.age} ans"
 
 event = Event.new("2019-01-06 14:00", 30, "standup quotidien",["truc@machin.com", "bidule@chose.fr"])
-puts event.start_date
-puts event.duration
-puts event.title
+# puts event.start_date
+# puts event.duration
+# puts event.title
+
 
 binding.pry 
 
--- lib/user.rb
+++ lib/user.rb
@@ -7,12 +7,20 @@ class User
   def initialize(email_to_save, age_to_save)
       @email = email_to_save # si l’e-mail passe le check => on le sauve dans l'instance
       @age = age_to_save
-      @@all_users << self.email
+      @@all_users << self
 
   end
 
+  def self.find_by_email(email_to_find)
+    @@all_users.each do | user |
+          return user if user.email == email_to_find
+    end
+  end
+
   def self.all
-    puts @@all_users
+    @@all_users.each do | user |
+      puts "#{user.email}"
+    end 
   end
 
 end
